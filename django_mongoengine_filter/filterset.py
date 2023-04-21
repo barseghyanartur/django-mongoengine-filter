a6699182629616a3208863b449813b498c9293d7
Oussama Jarrousse <oussama@jarrousse.org>
2023-04-21 02:13:25 +0200

    added is_valid() to BaseFilterSet class; fixing issue #8 allowing for integration with django-filters

--- django_mongoengine_filter/filterset.py
+++ django_mongoengine_filter/filterset.py
@@ -225,6 +225,13 @@ class BaseFilterSet:
     def __getitem__(self, key):
         return self.qs[key]
 
+    def is_valid(self):
+        """
+        Return True if the underlying form has no errors, or False otherwise.
+        """
+        return self.is_bound and self.form.is_valid()
+
+
     @property
     def qs(self):
         if not hasattr(self, "_qs"):
